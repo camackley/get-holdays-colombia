4806baa3b12c0a786340aae3d93b50e13dd0a1db
Mackley Carmona <desarrollo@controlmovil.net>
2020-06-10 16:56:34 -0500

    Add readme.md and comments

---
+++ README.md
@@ -0,0 +1,3 @@
+# Holidays in Colombia 2020
+
+This little script use holidays library of pip, if you wanna use this, you have to change the year and the country, for more information go to [Project description](https://pypi.org/project/holidays/)
--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 from datetime import date
 
+#Import library for get holidays
 import holidays
 
-if __name__ == "__main__":
-    co_holidays = holidays.CountryHoliday('CO')
-    
+def get_all_year():
     year_days = [[],[],[],[],[],[],[],[],[],[],[],[]]
-    
     i=1
     j=0
-    monts_31 = [1,3,5,7,8,10,12]
+    months_31 = [1,3,5,7,8,10,12]
     for month in year_days:
-        if i == monts_31[j]:
+        if i == months_31[j]:
             for day in range(1, 32):
                 month.append(day)
             j+=1
@@ -22,17 +20,23 @@ if __name__ == "__main__":
             for day in range(1, 31):
                 month.append(day)
         i+=1
-    #print(year_days)
+    return year_days
+
+if __name__ == "__main__":
+    #Instance Holidays for a specific country
+    co_holidays = holidays.CountryHoliday('CO')
+    
+    #Get all 2020 calendar for do a evaluation 
+    year_days = get_all_year()
     
     holidays_list=[]
     i=1
     for month in year_days:
         for day in month:
+            #Use a date for comparation with co_holdays object
             if date(2020, i, day) in co_holidays:
                 holidays_list.append(f'2020/{i}/{day}')
         i+=1
     
     for holiday in holidays_list:
         print(holiday)
-            
-                
